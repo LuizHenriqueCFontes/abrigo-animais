bcca80476bbac1df1105ea6aebaf1fdf3422d7d2
Luiz <luizhenrique3781@gmail.com>
2026-05-12 08:41:05 -0300

    feat: add icon bars

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,15 @@
-import Logo from "../Logo/Logo";
-
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPaw } from "@fortawesome/free-solid-svg-icons";
+import Logo from "../Logo/Logo";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+
+
 
 export default function Header(){
     return(
         <header>
             <Logo />
 
-            <nav>
-                <ul>
-                    <li><FontAwesomeIcon icon={faPaw} /></li>
-                </ul>
-            </nav>
+            <FontAwesomeIcon icon={faBars} />
         </header>
     );
 }
