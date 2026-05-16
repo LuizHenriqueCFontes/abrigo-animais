9a5774bebb95c2d3ea4eea49afcb6e828cacd8d0
Luiz <luizhenrique3781@gmail.com>
2026-05-15 21:23:44 -0300

    feat: complete hero section

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -2,23 +2,27 @@ import { useState } from "react";
 import dog from "../../assets/hero/dog-hero.png";
 import Input from "../Input/Input";
 import Button from "../Button/Button";
+import styles from "./Hero.module.css";
 
 export default function Hero(){
     const [find, setFind] = useState("");
 
     return(
-        <article>
-            <img src={dog} alt="Cachorro esperando ser adotado" />
+        <article className={styles.heroContainer}>
+            <img className={styles.dog} src={dog} alt="Cachorro esperando ser adotado" />
 
-            <section>
-                <h1>Adote seu cachorro, para ser seu melhor amigo a todo momento</h1>
-                <p>Centenas de cães e gatos em abrigos locais esperam por um lar amoroso. Encontre o seu par perfeito</p>
+            <section className={styles.titles}>
+                <h1 className={styles.heroTitle}><span className={styles.titleCostumizer}>Adote</span> seu cachorro, para ser seu melhor amigo a todo momento</h1>
+
+                <p className={styles.heroDescription}>Centenas de cães e gatos em abrigos locais esperam por um lar amoroso. Encontre o seu par perfeito</p>
             </section>
 
             <section>
-                <Input ariaLabel="Pesquisar abrigo ou cidade" id="find" value={find} onChange={setFind} placeholder="Pesquisar por Cidade ou Abrigo" variant="hero"/>
+                <form>
+                    <Input className={styles.form} ariaLabel="Pesquisar abrigo ou cidade" id="find" value={find} onChange={setFind} placeholder="Pesquisar por Cidade ou Abrigo" variant="hero"/>
 
-                <Button variant="primary">Encontrar Amigo</Button>
+                    <Button className={styles.form} variant="primary">Encontrar Amigo</Button>
+                </form>
             </section>
         </article>
 
